286619f65155e7101b57021ba73000b49699998c
Michelle Chen <michelle@Michelles-MacBook-Pro.local>
2022-02-15 18:34:39 -0500

    map integration

--- mushbuddy-app/components/map.js
+++ mushbuddy-app/components/map.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 class Map extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Map here.
-        </Text>
+        <MapView
+          initialRegion={{
+            latitude: 43.700859,
+            longitude: -72.289398,
+            latitudeDelta: 0.0,
+            longitudeDelta: 0.0,
+          }}
+          style={styles.map} />
       </View>
     );
   }
@@ -16,12 +22,13 @@ class Map extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  image: {
-    width: 400,
-    height: 300,
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
 
