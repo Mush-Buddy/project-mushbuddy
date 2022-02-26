96ec8ea33077807644422fef7a58f6b4f4adf2b6
Michelle Chen <michelle@Michelles-MacBook-Pro.local>
2022-02-26 14:35:05 -0500

    upload button

--- mushbuddy-app/components/map_post.js
+++ mushbuddy-app/components/map_post.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Pressable } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import DropDownPicker from 'react-native-dropdown-picker';
 
@@ -25,7 +25,7 @@ const MapPost = ({ navigation }) => {
     const [page, setPage] = useState(1);
 
     // input field setup
-    //const [nameText, setNameText] = useState('');
+    const [nameText, setNameText] = useState('');
 
     useEffect(async () => {
         const res = await getDataAPI(`catalog/?page=${page}&limit=${limit}`, auth.token);
@@ -37,31 +37,16 @@ const MapPost = ({ navigation }) => {
         }
     }, []); // this empty array (second arg.) prevents getDataAPI from being called every time a component updates
 
-    const handleLoadMore = async () => {
-        const res = await getDataAPI(`catalog/?page=${page}`, auth.token);
-        const newData = res.data.catalog;
-        if (newData.length > 0) {
-            setData((c) => {
-                return c.concat(newData);
-            });
-            setPage(page + 1);
-        }
-    }
-
-    const loadDataIntoItems = (catalogData) => {
-        const currentData = catalogData;
-        let loadedItems = items;
-        if (typeof currentData !== 'undefined' && currentData.length > 0) {
-            for (var i = 0; i < currentData.length; i++) {
-                loadedItems.push({
-                    label: currentData[i].nameCommon,
-                    value: currentData[i]._id,
-                });
-            }
-        }
-        console.log(loadedItems);
-        return loadedItems;
-    }
+    // const handleLoadMore = async () => {
+    //     const res = await getDataAPI(`catalog/?page=${page}`, auth.token);
+    //     const newData = res.data.catalog;
+    //     if (newData.length > 0) {
+    //         setData((c) => {
+    //             return c.concat(newData);
+    //         });
+    //         setPage(page + 1);
+    //     }
+    // }
 
     const returnToMap = () => {
         navigation.navigate('MapMain');
@@ -78,6 +63,25 @@ const MapPost = ({ navigation }) => {
         );
     }
 
+    // DROPDOWN PICKER --
+    // Populating with duplicate values from what I assume is too many API calls
+    // setItems is currently being called in useEffect
+
+    const loadDataIntoItems = (catalogData) => {
+        const currentData = catalogData;
+        let loadedItems = items;
+        if (typeof currentData !== 'undefined' && currentData.length > 0) {
+            for (var i = 0; i < currentData.length; i++) {
+                loadedItems.push({
+                    label: currentData[i].nameCommon,
+                    value: currentData[i]._id,
+                });
+            }
+        }
+        console.log(loadedItems);
+        return loadedItems;
+    }
+
     const renderCatalogDropdown = () => {
         return (
             <DropDownPicker
@@ -91,6 +95,41 @@ const MapPost = ({ navigation }) => {
                 searchablePlaceholder="Search..."
                 searchableError="No existing entries match your search."
             />
+        );
+    }
+    
+    // INPUT FIELD --
+    // Using this as a substitute for dropdown picker until that gets fixed.
+
+    const renderInputField = () => {
+        return(
+            <TextInput
+                style={styles.inputText}
+                autoCapitalize="none"
+                placeholder="Name of your mushroom..."
+                placeholderTextColor="#777777"
+                onChangeText={(newNameText) => setNameText(newNameText)}
+                defaultValue={nameText}
+                maxLength={40}
+            />
+        );
+    }
+
+    // TODO:
+    // (1) Make the post (API call)**
+    // (2) Return to map (already there, see below)
+    const handleOnPressUpload = () => {
+        returnToMap();
+    }
+
+    const renderUploadButton = () => {
+        // TODO: Should first check if all fields have been completed
+        return (
+            <Pressable style={styles.uploadButton} onPress={handleOnPressUpload}>
+                <Text style={styles.buttonText}>
+                    Upload
+                </Text>
+            </Pressable>
         );
     }
 
@@ -109,19 +148,22 @@ const MapPost = ({ navigation }) => {
 
                 <View style={styles.inputFieldIndividual}>
                     <Text style={styles.subheaderText}>
-                        Name of the mushroom
+                        Mushroom name
                     </Text>
-                    {/* <TextInput
-                            style={styles.inputText}
-                            autoCapitalize="none"
-                            placeholder="Name of your mushroom..."
-                            placeholderTextColor="#777777"
-                            onChangeText={(newNameText) => setNameText(newNameText)}
-                            defaultValue={nameText}
-                            maxLength={40}
-                        /> */}
-                    {renderCatalogDropdown()}
+                    {renderInputField()}
+                    {/* {renderCatalogDropdown()} */}
                 </View>
+
+                <View style={styles.inputFieldIndividual}>
+                    <Text style={styles.subheaderText}>
+                        Location found
+                    </Text>
+                </View>
+
+            </View>
+
+            <View style={styles.footerContainer}>
+                    {renderUploadButton()}
             </View>
         </View>
     );
@@ -130,6 +172,22 @@ const MapPost = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#F0ECE8',
+    },
+    headerContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        marginTop: 15,
+        marginLeft: 10,
+    },
+    footerContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginRight: 20,
     },
     inputFieldsContainer: {
         flex: 1,
@@ -145,19 +203,11 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'flex-start',
     },
-    headerContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        marginTop: 15,
-        marginLeft: 10,
-    },
     headerText: {
         fontSize: 22.5,
         fontWeight: 'bold',
         color: '#222222',
-        letterSpacing: 0.8,
+        letterSpacing: 1,
     },
     subheaderText: {
         fontSize: 18,
@@ -170,6 +220,10 @@ const styles = StyleSheet.create({
         color: '#222222',
     },
     inputText: {
+        height: 30,
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 5,
         fontSize: 12,
         color: '#222222',
     },
@@ -178,6 +232,20 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         position: 'absolute',
         left: 5,
+    },
+    uploadButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 100,
+        height: 37.5,
+        borderRadius: 4,
+        backgroundColor: '#59A896',
+    },
+    buttonText: {
+        fontSize: 14,
+        letterSpacing: 1,
+        color: 'white',
+        fontWeight: 'bold',
     },
 });
 
