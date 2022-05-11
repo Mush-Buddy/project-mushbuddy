6ddae95d74ac790b0d1360403043992d7c9b1863
Anshul Barnwal <anshul.barnwal.21@dartmouth.edu>
2022-05-10 22:31:46 -0400

    removing backslash

--- updated-mushbuddy-app/src/utils/config.js
+++ updated-mushbuddy-app/src/utils/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://mushbuddy-backend.herokuapp.com/"
+export const BASE_URL = "https://mushbuddy-backend.herokuapp.com"
--- updated-mushbuddy-app/utils/config.js
+++ updated-mushbuddy-app/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://mushbuddy-backend.herokuapp.com/"
+export const BASE_URL = "https://mushbuddy-backend.herokuapp.com"
 // export const BASE_URL = ""
