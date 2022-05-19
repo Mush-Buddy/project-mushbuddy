6407acc78b816758f6bbe0d07d08b24e93215c94
Michelle Chen <michelle.chen.22@dartmouth.edu>
2022-05-19 17:00:27 -0400

    marker callout setup

--- updated-mushbuddy-app/src/screens/map/markers.js
+++ updated-mushbuddy-app/src/screens/map/markers.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import { Text, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MapView, { Marker, Callout } from 'react-native-maps';
+// import Modal from 'react-native-modal';
 
 // a collection of markers.
 // takes in a function to be invoked after dragging a marker.
-const Markers = ( {markers, onDragEndEvent} ) => {
+const Markers = ({ markers, onDragEndEvent }) => {
     return (
         <>
             {/* renders an array of markers sequentially. */}
             {markers.map((markers, index) => (
-                <Marker 
+                // <Marker 
+                //     id={markers.id}
+                //     key={index}
+                //     title={markers.title}
+                //     description={markers.description}
+                //     mushroom={markers.mushroom}
+                //     coordinate={markers.coordinate}
+                //     draggable={true}
+                //     onDragEnd={(e) => onDragEndEvent(index, e.nativeEvent.coordinate)}
+                // /> 
+                <Marker
                     id={markers.id}
                     key={index}
                     title={markers.title}
@@ -17,7 +30,45 @@ const Markers = ( {markers, onDragEndEvent} ) => {
                     coordinate={markers.coordinate}
                     draggable={true}
                     onDragEnd={(e) => onDragEndEvent(index, e.nativeEvent.coordinate)}
-                /> 
+                >
+                    <Callout
+                        onPress={() => {
+                            console.log("callout pressed");
+                        }}>
+                        <View style={{
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+
+                            <Text style={{
+                                fontSize: 12,
+                                fontWeight: 'bold',
+                            }}>
+                                {markers.title}
+                            </Text>
+                            <Text style={{
+                                fontSize: 10,
+                                fontStyle: 'italic',
+                            }}>
+                                {markers.description}
+                            </Text>
+                            {/* <Text>
+                                    {Object.values(markers.mushroom)}
+                                </Text>
+
+                                <Text>
+                                    {Object.values(markers.coordinate)}
+                                </Text> */}
+                            {/* <TouchableOpacity
+                                    onPress={}
+                                >
+
+                                </TouchableOpacity> */}
+                        </View>
+
+                    </Callout>
+                </Marker>
             ))}
         </>
     )
